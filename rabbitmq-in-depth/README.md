3d3704ef5a7f305f3b7183efe108d46d9d0c413c
sieunkim <sieunkr@gmail.com>
2018-08-31 23:03:17 +0900

    mandatory 설정

---
+++ rabbitmq-in-depth/README.md
@@ -0,0 +1,29 @@
+
+
+## RabbitMQ 샘플 소스 모음
+스터디 중 작성한 코드를 간단하게 정리한다. 모든 코드는 아래 라이브러리를 사용한다.
+- pika
+- rabbitpy
+
+## RabbitMQ 기본 연동
+- 자세한 설명은 생략한다.
+- consume_pika.py
+- consume_rabbipy.py
+- publish_pika.py
+- publish_rabbitpy.py
+
+## mandatory 플래그 설정
+
+- publish_mandatory_rabbitpy.py
+
+익스체인지가 메시지를 라우팅할 수 없다면, 메시지를 되돌려 보내도록 하기 위해서는 mandatory=True 를 설정하면 된다.
+````python
+message.publish(exchange, 
+                'example-routing-key-not',
+                mandatory=True)
+````
+실행하면 아래와 같은 메시지가 출력된다. 
+
+rabbitpy.exceptions.MessageReturnedException: Message was returned by RabbitMQ: (312) for exchange NO_ROUTE
+
+
---
+++ rabbitmq-in-depth/publish_mandatory_rabbitpy.py
@@ -0,0 +1,26 @@
+import rabbitpy
+
+url = 'amqp://u:p@url:5672/%2F'
+
+connection = rabbitpy.Connection(url)
+
+channel = connection.channel()
+
+exchange = rabbitpy.Exchange(channel, 'exchage.01')
+
+exchange.declare()
+
+queue = rabbitpy.Queue(channel, 'queue.01')
+queue.declare()
+queue.bind(exchange, 'example-routing-key')
+
+message = rabbitpy.Message(channel,
+                               'Test message',
+                               {'content_type': 'text/plain'},
+                               opinionated=True)
+
+message.publish(exchange, 
+                'example-routing-key-not',
+                mandatory=True)
+
+connection.close()
